3c743e5fa96d711ac486fd9c7d515b96cf2b0945
Vicente <vic.bravoc@duocuc.cl>
2025-05-04 12:35:00 -0400

    cambios

---
+++ proyecto_spring_personal/src/main/java/controller/TareaController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class TareaController {
+    @GetMapping()
+    public String getStatus() {
+        return "El ms TodoApp esta operativo 🥂";
+    }
+    
+}
