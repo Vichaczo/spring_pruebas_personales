0ac43907f3555136a9331a7a0ba48629323bd4dc
Vicente <vic.bravoc@duocuc.cl>
2025-05-04 12:52:22 -0400

    Se creo el controlador de status

--- proyecto_spring_personal/src/main/java/com/proyecto_spring_personal/proyecto_spring_personal/controller/TareaController.java
+++ proyecto_spring_personal/src/main/java/com/proyecto_spring_personal/proyecto_spring_personal/controller/TareaController.java
@@ -1,13 +1,15 @@
-package controller;
+package com.proyecto_spring_personal.proyecto_spring_personal.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("/api/v1/todoapp")
 public class TareaController {
-    @GetMapping()
+    @GetMapping("/status")
     public String getStatus() {
         return "El ms TodoApp esta operativo 🥂";
     }
