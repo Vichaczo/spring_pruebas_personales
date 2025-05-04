e528cb766227bc8e0ab47a6082e6fb955f19f4f7
Vicente <vic.bravoc@duocuc.cl>
2025-05-03 22:26:56 -0400

    Creacion del proyecto spring

---
+++ proyecto_spring_personal/src/main/java/com/proyecto_spring_personal/proyecto_spring_personal/ProyectoSpringPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_spring_personal.proyecto_spring_personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringPersonalApplication.class, args);
+	}
+
+}
